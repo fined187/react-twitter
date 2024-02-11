0ef0d5390c70d3fdb63fa46f978f42c1f9953d53
wootaiklee <fined000@naver.com>
2024-02-11 18:44:03 +0900

    media query test

---
+++ src/constants/language.ts
@@ -0,0 +1,13 @@
+const TRANSLATIONS = {
+  //  menu
+  MENU_HOME: {
+    ko: '홈',
+    en: 'Home',
+  },
+  MENU_PROFILE: {
+    ko: '프로필',
+    en: 'Profile',
+  },
+}
+
+export default TRANSLATIONS
